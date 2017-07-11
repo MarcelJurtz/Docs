2c77df731b40793be7d4972e320427dd7f0af779
MarcelJurtz <jurtzmarcel@gmail.com>
2017-07-11 13:40:55 +0200

    Add kotlin folder and hello world demo

---
+++ Kotlin/HelloKotlin/app/src/main/java/com/marceljurtz/hellokotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.marceljurtz.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lblHelloWorld.setText("Hello Kotlin!")
+    }
+}
